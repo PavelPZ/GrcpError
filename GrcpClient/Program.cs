cf0a2cee2fee18cc95cd4116e7e0e7c356e4d385
Pavel Zika <pzika@langmaster.cz>
2019-08-04 14:32:30 +0200

    fix

--- GrcpClient/Program.cs
+++ GrcpClient/Program.cs
@@ -1,20 +1,17 @@
 ﻿using System;
 
-namespace GrcpClient {
+namespace GrcpClient { 
   class Program {
     static void Main(string[] args) {
-      var client = new GrpcServer.Greeter.GreeterClient(getInvoker());
+      var channel = new Grpc.Core.Channel("localhost", 5001, Grpc.Core.ChannelCredentials.Insecure);
+      var invoker = new Grpc.Core.DefaultCallInvoker(channel);
+      var client = new GrpcServer.Greeter.GreeterClient(invoker);
       try {
         var res = client.SayHello(new GrpcServer.HelloRequest { Name = "Hello name" });
         Console.WriteLine(res.Message);
       } catch (Exception exp) {
         if (exp == null) return;
       }
-    }
-
-    public static Grpc.Core.CallInvoker getInvoker() {
-      var channel = new Grpc.Core.Channel("localhost", 5001, Grpc.Core.ChannelCredentials.Insecure);
-      return new Grpc.Core.DefaultCallInvoker(channel);
     }
 
   }
